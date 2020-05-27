d699693d8b8b3a2a33425d9cc6b460bc97370b68
Eli Weinberger <ebweinberger@ElisMBP.fios-router.home>
2020-05-26 22:25:35 -0400

    Added page-header component and put my name at the top of the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PageHeaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
